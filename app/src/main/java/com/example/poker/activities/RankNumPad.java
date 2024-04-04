8d42d7a736ce90cb12a1963715f6fa31546a6951
andrewklein98 <andrewklein98@gmail.com>
2024-04-04 18:41:20 +0100

    button rubbish

--- app/src/main/java/com/example/poker/activities/RankNumPad.java
+++ app/src/main/java/com/example/poker/activities/RankNumPad.java
@@ -1,9 +1,6 @@
-package com.example.poker;
+package com.example.poker.activities;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,24 +8,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.lang.reflect.Array;
+import com.example.poker.R;
 
-public class MainActivity extends AppCompatActivity {
+public class RankNumPad extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_rank_num_pad);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        String[] suit_list = new String[]{"DIAMONDS","HEARTS","SPADES","CLUBS"};
-        Spinner suit_dropdown = findViewById(R.id.suit_spinner);
-        ArrayAdapter<String> suit_adapter =new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item,suit_list);
-        suit_dropdown.setAdapter(suit_adapter);
     }
 }
